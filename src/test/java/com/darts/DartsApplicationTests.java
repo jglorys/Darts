f0bc5d94edf651adab9bf43b2f3b4a2aa6fc2c81
Ella <38400022+jglorys@users.noreply.github.com>
2023-08-17 13:25:03 +0900

    DARTS FIRST COMMIT

---
+++ src/test/java/com/darts/DartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
